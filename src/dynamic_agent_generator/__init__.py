4894cd621943f3b24a46bb0cdb793127bd295497
taheralibadnawarwala <tahercoolguy@gmail.com>
2025-01-15 17:42:42 +0530

    Added new files

--- src/dynamic_agent_generator/__init__.py
+++ src/dynamic_agent_generator/__init__.py
@@ -1,4 +1,7 @@
 from .agent_generator import DynamicAgentGenerator
 
+# Allow both names to be used
+AgentGenerator = DynamicAgentGenerator
+
 __version__ = "0.1.0"
-__all__ = ["DynamicAgentGenerator"] 
+__all__ = ["DynamicAgentGenerator", "AgentGenerator"] 
